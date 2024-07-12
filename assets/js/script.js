e322157ccad99854258567631b9e8412c5ddf2a3
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-07-12 15:47:39 +0200

    commitFinish

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,21 @@ function stickyHeader() {
         getS.style.backgroundColor = "#181919"
     }
 }
+
+// animazione
+const gElements = document.getElementsByTagName("g");
+const gElementsFiltered = Array.from(gElements).filter(
+  (element) => element.getAttribute("opacity") === "0"
+);
+gElementsFiltered.shift();
+
+const gAnimation = setInterval(() => {
+  const randomNum = Math.floor(Math.random() * gElementsFiltered.length);
+  const currentElement = gElementsFiltered[randomNum];
+  const currentOpacity = currentElement.getAttribute("opacity");
+  if (currentOpacity === "0") {
+    currentElement.setAttribute("opacity", 1);
+  } else if (currentOpacity === "1") {
+    currentElement.setAttribute("opacity", 0);
+  }
+}, 50);
